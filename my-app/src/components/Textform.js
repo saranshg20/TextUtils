bfa0f8b816201229c3f690562617977afae76e56
saransh_477 <saranshg20@iitk.ac.in>
2022-03-15 11:08:23 +0530

    Adding more logic to the TextUtils web application

--- my-app/src/components/Textform.js
+++ my-app/src/components/Textform.js
@@ -1,27 +1,43 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Textform(props) {
     const [text, setText] = useState('Enter Text Here');
 
-    const handleUpClick=()=>{
+    const handleUpClick = () => {
         console.log("Button was Clicked");
         let newText = text.toUpperCase();
         setText(newText);
     }
 
-    const handleOnChange=(event)=>{
+    const handleLoClick = () => {
+        console.log("Button was Clicked");
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleOnChange = (event) => {
         console.log("Handle on Change");
         setText(event.target.value);
     }
 
     // setText("New Text");
-  return (
-    <>
-  <label htmlFor="text-area" className="form-label">{props.heading}</label>
-        <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
-    </div>
-  <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <h4>{props.heading}</h4>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                </div>
+                <button className='btn btn-primary' onClick={handleUpClick}>Convert to UpperCase</button>
+                <button className='btn btn-primary mx-2' onClick={handleLoClick}>Convert to LowerCase</button>
+            </div>
+            <div className="container my-3">
+                <h4>Text Summary</h4>
+                <p>Text has {text.length} characters and {text.split(" ").length} words</p>
+                <p>Reading time around {0.008 * (text.split(" ").length)} minutes.</p>
+                <h4>Preview</h4>
+                <p>{text}</p>
+            </div>
+        </>
+    )
 }
